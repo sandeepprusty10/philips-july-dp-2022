38c400854af0e15dae199ca27f793232b10bfddc
sandeepprusty10 <44285578+sandeepprusty10@users.noreply.github.com>
2022-08-04 21:44:34 +0530

    Added Assg6

---
+++ AssignmentSheet/Assg6.cs
@@ -0,0 +1,31 @@
+﻿public interface ICalculator
+{
+    int Add(int x, int y);
+}
+public class ConcreteCalculator : ICalculator
+{
+    public int Add(int x, int y)
+    {
+
+        var addition = x + y;
+        return addition;
+    }
+}
+
+public class Printer : ICalculator
+{
+    private ICalculator _calculator;
+    public Printer(ICalculator calculator)
+    {
+        _calculator = calculator;
+    }
+
+    public int Add(int x, int y)
+    {
+        int value =  _calculator.Add(x, y);
+        Print("result ={0}", value);
+        return value;
+    }
+
+    private void Print(string statement, int value) { }
+}
